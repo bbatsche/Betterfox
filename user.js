1580428118e1c3b5c22ba3e697b831f649c8ce41
Ben Batschelet <bartleby84@gmail.com>
2024-03-13 12:42:01 -0500

    Temp dir interferes with unarchiver behavior

--- user.js
+++ user.js
@@ -94,7 +94,6 @@ user_pref("browser.contentblocking.category", "strict");
 user_pref("urlclassifier.trackingSkipURLs", "*.reddit.com, *.twitter.com, *.twimg.com, *.tiktok.com");
 user_pref("urlclassifier.features.socialtracking.skipURLs", "*.instagram.com, *.twitter.com, *.twimg.com");
 user_pref("network.cookie.sameSite.noneRequiresSecure", true);
-user_pref("browser.download.start_downloads_in_tmp_dir", true);
 user_pref("browser.helperApps.deleteTempFileOnExit", true);
 user_pref("browser.uitour.enabled", false);
 user_pref("privacy.globalprivacycontrol.enabled", true);
